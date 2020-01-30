4152f1ae9c969ece24bd368718ebca7c092dffe9
Chloe Foronda <caforonda103@yahoo.com>
2020-01-30 23:57:56 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,13 +2,12 @@ require 'pry'
 
 class Backer
   
-  attr_accessor :name
+  attr_accessor :name, :backed_projects
   
-  @@backed_projects = []
   
   def initialize(name)
     @name = name
-    @@backed_projects << self
+    @backed_projects << self
   end
   
 end
